6499bb468eba6481fe0bebd241d18fa2d6a2f9f4
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-02-20 22:03:52 +0100

    update axios settings

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+if (process.env.REACT_APP_API_ACCESS === "LOCAL") {
   axios.defaults.baseURL = "http://127.0.0.1:8000/api";
 } else {
   axios.defaults.baseURL = "https://pp5-drf-task-api.herokuapp.com/api";
